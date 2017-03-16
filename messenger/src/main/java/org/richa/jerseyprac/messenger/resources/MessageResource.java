8e3ffa097aa3b4dc8adf6e77188693bb9d41e5e2
Richa Bhatia <bhatia.ri@husky.neu.edu>
2017-03-16 01:57:23 -0400

    Added update and delete endpoints

--- messenger/src/main/java/org/richa/jerseyprac/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/richa/jerseyprac/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package org.richa.jerseyprac.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,5 +44,34 @@ public class MessageResource {
 	{
 		
 	return messageService.getMessage(messageId);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message m)
+	{
+		return messageService.addMessage(m);
+		
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message)
+	{
+		message.setId(messageId);
+	return messageService.updateMessage(message);
+	}
+	
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long messageId)
+	{
+		
+	 messageService.removeMessage(messageId);
 	}
 }
