d7d362b8711ce280311c8724722689006d7c2922
Richa Bhatia <bhatia.ri@husky.neu.edu>
2017-03-16 11:51:37 -0400

    Implemented HATEAOS concepts using rel paths

---
+++ messenger/src/main/java/org/richa/jerseyprac/messenger/model/Link.java
@@ -0,0 +1,35 @@
+package org.richa.jerseyprac.messenger.model;
+
+public class Link {
+
+	private String link;
+	private String rel;
+	
+	
+	/**
+	 * @return the link
+	 */
+	public String getLink() {
+		return link;
+	}
+	/**
+	 * @param link the link to set
+	 */
+	public void setLink(String link) {
+		this.link = link;
+	}
+	/**
+	 * @return the rel
+	 */
+	public String getRel() {
+		return rel;
+	}
+	/**
+	 * @param rel the rel to set
+	 */
+	public void setRel(String rel) {
+		this.rel = rel;
+	}
+	
+	
+}
--- messenger/src/main/java/org/richa/jerseyprac/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/richa/jerseyprac/messenger/resources/MessageResource.java
@@ -11,7 +11,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 import org.richa.jerseyprac.messenger.model.Message;
 import org.richa.jerseyprac.messenger.service.MessageService;
@@ -54,10 +56,23 @@ public class MessageResource {
 	@GET
 	@Path("/{messageId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Message getMessage(@PathParam("messageId") long messageId)
+	public Message getMessage(@PathParam("messageId") long messageId, @Context UriInfo uriInfo)
 	{
+		Message message =messageService.getMessage(messageId);
 		
-	return messageService.getMessage(messageId);
+		String uri = getUriForSelf(uriInfo, message); 
+		
+		message.addLink(uri, "self");
+		return message;
+	}
+
+	private String getUriForSelf(UriInfo uriInfo, Message message) {
+		String uri = uriInfo.getBaseUriBuilder()     //this gives http://localhost:8080/messenger/webapi
+		.path(MessageResource.class)                 //this gives /messages
+		.path(Long.toString(message.getId()))
+		.build()
+		.toString();
+		return uri;
 	}
 	
 	@POST
